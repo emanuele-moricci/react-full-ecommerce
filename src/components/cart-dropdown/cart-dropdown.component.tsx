2fd9045955c2689b84fd84aae1fbc9dba96aace8
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-29 16:14:36 +0100

    Created the CartItem component and connected it through Redux

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,12 +1,29 @@
+import { connect } from "react-redux";
+import { CartItem as CartItemType } from "../../redux/cart/cart.types";
+import { RootState } from "../../redux/root.store";
+
+import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+interface ICartDropdownProps {
+  items: CartItemType[];
+}
+
+const CartDropdown = ({ items }: ICartDropdownProps) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {items.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { items } }: RootState) => ({
+  items,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,23 @@
+import { CartItem as CartItemType } from "../../redux/cart/cart.types";
+
+import "./cart-item.styles.scss";
+
+interface ICartItemProps {
+  item: CartItemType;
+}
+
+const CartItem = ({
+  item: { imageUrl, price, name, quantity },
+}: ICartItemProps) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
